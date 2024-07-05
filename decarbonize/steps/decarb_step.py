5fedb863b8bfb844af9033294b29e5bcb00c19a4
wjd2000 <wjd2000@berkeley.edu>
2024-07-05 11:49:40 -0700

    current price

--- decarbonize/steps/decarb_step.py
+++ decarbonize/steps/decarb_step.py
@@ -1,6 +1,6 @@
 
-class DecarbStep:
-    def __init__(self, step_type, user_current_plan, cur_cost, new_cost, cur_emissions, new_emissions, description, difficulty):
+class DecarbElectricStep:
+    def __init__(self, step_type, user_current_plan, cur_cost, new_cost,  cur_renewable, new_renewable, description, difficulty):
         self.step_type = step_type
         self.cur_cost = cur_cost
         self.new_cost = new_cost
@@ -10,6 +10,14 @@ class DecarbStep:
         self.difficulty = difficulty
         self.user_current_plan = user_current_plan
         
+    def get_cur_cost(self):
+        user_zip_code = 95347
+        ce = CurrentElectricity('Electricity Rate Plan.xlsx', user_zip_code)
+        user_current_plan = 'B19SVB'
+        new_cost = ce.check_condition_and_run(user_current_plan)
+        self.steps.append(new_cost)
+        return cur_cost
+
     
     def get_new_cost(self):
         user_zip_code = 95347
