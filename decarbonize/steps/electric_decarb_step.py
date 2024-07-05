38a7b60675420bd54c6e419da344eb91eb933159
wjd2000 <wjd2000@berkeley.edu>
2024-07-05 15:29:02 -0700

    Update electric_decarb_step.py

--- decarbonize/steps/electric_decarb_step.py
+++ decarbonize/steps/electric_decarb_step.py
@@ -10,27 +10,41 @@ class ElectricDecarbStep(DecarbStep):
         cur_emissions = self.get_carbon_from_electric(kwh_used)
         new_emissions = self.get_new_carbon_from_electric(cur_emissions, cur_renewable, new_renewable)
         super().__init__(DecarbStepType.ELECTRICITY, cur_cost, new_cost, cur_emissions, new_emissions, description, difficulty)
+        self.steps = []
+        self.cur_cost = self.get_cur_cost(UseCCA)
+        self.new_cost = self.get_new_cost(HasCCA)
 
-    def get_cur_cost(self):
+    def get_cur_cost(self, UseCCA):
         ce = CurrentElectricity('Electricity Rate Plan.xlsx', user_zip_code)
         lcb_sector = lcb_usage_data
         smb_sector = smb_usage_data
-        cur_cost = ce.check_condition_and_run(user_current_plan)
-        self.steps.append(cur_cost)
+        if UseCCA == 'Yes':
+            cur_cost = #Gowri to do (with CCA)(e.x. user_current_bill_price * (ratio_of_CCA_A1_plan / ratio_of_PGE_A1_plan))
+            self.steps.append(cur_cost)
+        elif UseCCA == 'No':
+            cur_cost = ce.check_condition_and_run(user_current_plan)
+            self.steps.append(cur_cost)
+        else:
+            pass
         return cur_cost
 
-    def get_new_cost(self):
+    def get_new_cost(self, HasCCA):
         ew = ElectricityWork('Electricity Rate Plan.xlsx', user_zip_code)
         lcb_sector = lcb_usage_data
         smb_sector = smb_usage_data
         lcu_sector = lcu_usage_data
         smu_sector = smu_usage_data
-        new_cost = ew.check_condition_and_run(user_sector, user_bundled)
-        self.steps.append(new_cost)
+        if HasCCA == 'Yes':
+            new_cost = #Gowri to do(with CCA, switch plan switch company)
+            self.steps.append(new_cost)
+        elif HasCCA == 'No':
+            new_cost = ew.check_condition_and_run(user_sector, user_bundled)
+            self.steps.append(new_cost)
+        else:
+            pass
         return new_cost
 
     def compute_electricbill_savings(self):
-        current_electricbill_price = 10000
         return (self.cur_cost - self.new_cost)/self.cur_cost * current_electricbill_price
 
     def generate_step_description(self):
@@ -55,6 +69,9 @@ user_bundled = 'Yes'
 user_current_plan = 'B19_SV'
 kwh_used = 10000
 current_electricbill_price = 100000
+
+UseCCA = 'No'
+HasCCA = 'No'
 
 lcb_usage_data = LCBSector(162, 76, 181, 101, 61, 37, 9, 78, 65, 13, 29,
                            161, 25, 34, 112, 143, 15, 78, 134, 92, 67, 67,
