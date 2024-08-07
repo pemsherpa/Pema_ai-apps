b7a295b68995700cce0a201a90fd0f70f80b6302
Tunan Li <tunanli@TunandeMacBook-Air.local>
2024-08-08 04:02:36 +1000

    cru

--- decarbonize/steps/decarb_emissions_step.py
+++ decarbonize/steps/decarb_emissions_step.py
@@ -4,5 +4,6 @@ from steps.decarb_step_type import DecarbStepType
 class EmissionsDecarbStep(DecarbStep):
     def __init__(self, cur_cost, new_cost, cur_emissions, new_emissions, description, difficulty):
         super().__init__(DecarbStepType.EMISSIONS_FLIGHTS, cur_cost, new_cost, cur_emissions, new_emissions, description, difficulty)
+        
 
     
