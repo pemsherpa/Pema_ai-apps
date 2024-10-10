5f5f8e88908f2c8a312bcd03f7accb8a9c71b6f5
Emma-Jane Burns <emmajaneb04@berkeley.edu>
2024-10-09 22:37:11 -0700

    parsing from json object

--- decarbFrontEnd/src/app/decarb-journey/decarb-journey.component.ts
+++ decarbFrontEnd/src/app/decarb-journey/decarb-journey.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { DecarbShoppingCartComponent } from './decarb-shopping-cart/decarb-shopping-cart.component';
+//import yearlyQuarterlySteps from '...'
 
 @Component({
   selector: 'app-decarb-journey',
   standalone: true,
-  imports: [RouterOutlet, RouterModule,DecarbShoppingCartComponent, CommonModule],
+  imports: [RouterOutlet, RouterModule,DecarbShoppingCartComponent, CommonModule, /*yearlyQuarterlySteps*/],
   templateUrl: './decarb-journey.component.html',
   styleUrl: './decarb-journey.component.css'
 })
@@ -14,6 +15,25 @@ export class DecarbJourneyComponent {
   title = 'decarbonization-journey';
 
   isDropdownOpen: { [key: string]: boolean } = {};
+
+  // // Data for 2024, quarters 1, 2, and 3
+  // quartileData1: any;
+  // quartileData2: any;
+  // quartileData3: any;
+
+  // constructor() {
+  //   // Extract the relevant data for year 2024 and quarters 1, 2, and 3
+  //   this.quartileData1 = yearlyQuarterlySteps.find(
+  //     (entry: any) => entry.year === 2024 && entry.quarter === 1
+  //   );
+  //   this.quartileData2 = yearlyQuarterlySteps.find(
+  //     (entry: any) => entry.year === 2024 && entry.quarter === 2
+  //   );
+  //   this.quartileData3 = yearlyQuarterlySteps.find(
+  //     (entry: any) => entry.year === 2024 && entry.quarter === 3
+  //   );
+  //}
+
 
   // Define the quartile data with unique dropdowns for each quartile
   quartileData = [
