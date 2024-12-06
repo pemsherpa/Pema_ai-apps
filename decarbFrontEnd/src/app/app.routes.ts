d418171a16a4184b251790b8815267c4283f6c2b
gowrig <galgaligowri@gmail.com>
2024-12-06 15:50:01 +0530

    routes

--- decarbFrontEnd/src/app/app.routes.ts
+++ decarbFrontEnd/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { DecarbjourneyComponent } from './decarbjourney/decarbjourney.component'
 import { ClassifymapComponent } from './classifymap/classifymap.component';
 import { DecarbJourneyComponent } from './decarb-journey/decarb-journey.component';
 import { MakeSwitchComponent } from './decarb-journey/quartile-steps/make-switch/make-switch.component';
+import { AnomalyDetectionComponent } from './anomaly-detection/anomaly-detection.component';
 
 
 const routes: Routes = [
@@ -27,6 +28,10 @@ const routes: Routes = [
   path: 'make-switch',
   component: MakeSwitchComponent,
   title: 'Make-the-switch'
+},{
+  path:'anomaly-detection',
+  component:AnomalyDetectionComponent,
+  title:'Detect-anomalies'
 }
 
 ]; 
